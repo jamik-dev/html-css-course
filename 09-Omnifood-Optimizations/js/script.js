612157a710fc1ea2e1a7b39339447f09f75ff0df
jamik-dev <jamik.oik.group@gmail.com>
2023-07-25 23:53:49 +0500

    sticky navbar, scroll animation, more done

--- 09-Omnifood-Optimizations/js/script.js
+++ 09-Omnifood-Optimizations/js/script.js
@@ -7,12 +7,61 @@ yearEl.textContent = currentYear;
 const btnNav = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 
-btnNav.addEventListener('click', () => {
+btnNav.addEventListener("click", () => {
   headerEl.classList.toggle("nav-open");
 });
 
 ///////////////////////////////////////////////////////////
 // Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Sticky Navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const observer = new IntersectionObserver(function(entries) {
+  const ent = entries[0];
+  if (!ent.isIntersecting) {
+    document.body.classList.add('sticky');
+  }
+
+  if (ent.isIntersecting) {
+    document.body.classList.remove('sticky');
+  }
+}, {
+  // In the viewport
+  root: null,
+  threshold: 0,
+  rootMargin: '-80px'
+});
+observer.observe(sectionHeroEl);
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
